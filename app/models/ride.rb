3160dd8b4ab929823289175007faeadb5874df7f
lastromanticx <giladbarkan@live.com>
2016-07-24 16:09:29 -0400

    model spec mostly working

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -4,7 +4,21 @@ class Ride < ActiveRecord::Base
   belongs_to :user
 
   def take_ride
-    if 
-    "Sorry. You are not tall enough to ride the #{attraction.name}."
+    message = ""
+    if user.tickets < attraction.tickets
+      message += "Sorry. You do not have enough tickets the #{attraction.name}."
+    end
+    if user.height < attraction.min_height
+      message += (message == "" ? "Sorry." : "") + " You are not tall enough to ride the #{attraction.name}."
+    end
+    
+    if message == ""
+      user.tickets -= attraction.tickets
+      user.nausea += attraction.nausea_rating
+      user.happiness += attraction.happiness_rating
+      user.save
+    else
+      message
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
   # write associations here
+  has_many :rides
+  has_many :attractions, through: :rides
+  has_secure_password
+
+  def mood
+    if nausea > happiness
+      "sad"
+    elsif
+      happiness > nausea
+      "happy"
+    end
+  end
 end
