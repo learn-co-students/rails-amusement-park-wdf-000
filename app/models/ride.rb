9023b158fae1c5e0300382c0d6e4a6e46a6d0609
blessedsoy <unmigong@gmail.com>
2016-08-12 15:33:51 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,36 @@
 class Ride < ActiveRecord::Base
   # write associations here
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    message = "Sorry."
+    if !enough_tickets
+      message += " You do not have enough tickets the #{attraction.name}."
+    end
+    if !tall_enough
+      message += " You are not tall enough to ride the #{attraction.name}."
+    end
+
+    if enough_tickets && tall_enough
+      user.tickets -= attraction.tickets
+      user.happiness += attraction.happiness_rating
+      user.nausea += attraction.nausea_rating
+      user.save
+      "Thanks for riding the #{attraction.name}!"
+    else
+      message
+    end
+  end
+
+  private
+
+  def enough_tickets
+    user.tickets > attraction.tickets
+  end
+
+  def tall_enough
+    user.height > attraction.min_height
+  end
+
 end
