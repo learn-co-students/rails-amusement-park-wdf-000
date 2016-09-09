3f0ded4adb08813a34a0a418aaaca8630b8e8d30
Margaret <eutopia@gmail.com>
2016-09-09 11:10:26 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+	def create
+		@user = User.find_by(id: params[:user][:id])
+		if @user
+			session[:user_id] = @user.id
+			redirect_to user_path(@user)
+		else
+			redirect_to '/'
+		end
+	end
+
+	def destroy
+		session.delete :user_id
+		redirect_to '/'
+	end
+end
