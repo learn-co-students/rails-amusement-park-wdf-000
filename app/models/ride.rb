6c7b06bfefa7b6a8b5496eda480d758d206afa36
satub <satu.barnhill@gmail.com>
2016-09-20 16:25:59 -0400

    Attraction and Ride -model specs pass

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,20 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    if self.user.tickets - self.attraction.tickets < 0 && self.user.height < self.attraction.min_height
+      "Sorry. You do not have enough tickets for the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif self.user.tickets - self.attraction.tickets < 0
+      "Sorry. You do not have enough tickets for the #{self.attraction.name}."
+    elsif self.user.height < self.attraction.min_height
+      "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+    else
+      tickets_left = self.user.tickets - self.attraction.tickets
+      nausea_status = self.user.nausea +  self.attraction.nausea_rating
+      happiness_level = self.user.happiness +  self.attraction.happiness_rating
+      self.user.update(tickets: tickets_left, nausea: nausea_status, happiness:  happiness_level)
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-  # write associations here
+  has_many :rides
+  has_many :attractions, through: :rides
+  has_secure_password(validations: false)
 end
