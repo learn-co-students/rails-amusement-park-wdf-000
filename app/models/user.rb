1fac8a0f553b79da8f7433ea80afb3097508bf7f
fidelscodes <fidelscodes@gmail.com>
2016-10-03 17:43:01 -0400

    add #mood, all model tests passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   has_many :rides
   has_many :attractions, through: :rides
   has_secure_password
+
+  def mood
+    nausea > happiness ? "sad" : "happy"
+  end
+
 end
