2921d99229e9d4464d80b622ef6fe44ea65b0069
ozPop <ozziepopovas@gmail.com>
2016-10-04 17:30:31 -0400

    add users form partial, set_user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit]
   def new
+    @user = User.new
   end
 
   def create
@@ -15,5 +17,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(session[:user_id])
   end
 end
