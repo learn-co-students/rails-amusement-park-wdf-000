b6d0a23767551f1db17e6f534ce324753a26b848
ozPop <ozziepopovas@gmail.com>
2016-10-04 16:54:25 -0400

    refactor signin form, update sessions actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    return redirect_to :back if user_params[:id].blank?
+    session[:user_id] = user_params[:id]
+    redirect_to user_path(session[:id])
   end
 
   def destroy
+    session.delete :user_id
+    redirect_to root_path, alert: 'You have logged out'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id)
   end
 end
