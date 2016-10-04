7c0ef72d7f678642de65c229ef987c098ed3178c
ozPop <ozziepopovas@gmail.com>
2016-10-04 14:58:56 -0400

    update models with associations and methods

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,31 @@
 class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    user = User.find(user_id)
+    attraction = Attraction.find(attraction_id)
+    processed = process_requirements(user, attraction)
+    return processed unless processed == 'pass'
+    update_attributes(user, attraction)
+  end
+
+  def process_requirements(user, attraction)
+    if user.tickets < attraction.tickets && user.height < attraction.min_height
+      return "Sorry. You do not have enough tickets the #{attraction.name}."\
+      + ' You are not tall enough to ride the Roller Coaster.'
+    elsif user.height < attraction.min_height
+      return "Sorry. You are not tall enough to ride the #{attraction.name}."
+    elsif user.tickets < attraction.tickets
+      return "Sorry. You do not have enough tickets the #{attraction.name}."
+    end
+    'pass'
+  end
+
+  def update_attributes(user, attraction)
+    user.tickets -= attraction.tickets
+    user.nausea += attraction.nausea_rating
+    user.happiness += attraction.happiness_rating
+    user.save
+  end
 end
