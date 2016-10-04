6225be3882181e50458428441a9236f463c4aa15
ozPop <ozziepopovas@gmail.com>
2016-10-04 18:06:24 -0400

    update user form, create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    redirect_to :back unless @user.save
+    session[:user_id] = @user.id
+    redirect_to user_path(session[:user_id])
   end
 
   def show
@@ -23,5 +27,9 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(session[:user_id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets, :password, :admin)
   end
 end
