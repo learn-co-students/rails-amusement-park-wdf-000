93d1cfd3fd72e50db945900bed0ed79d30e598c1
fidelscodes <fidelscodes@gmail.com>
2016-10-04 10:49:06 -0400

    add login as non-admin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    # use guard clause here?
+    user = User.find_by(id: params[:user][:id])
+    session[:user_id] = user.id
+    redirect_to user_path(user)
+  end
+
+  # destroy session
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: 'welcome#home'
 
   get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+
   resources :users
 end
