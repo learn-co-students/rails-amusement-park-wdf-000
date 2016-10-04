bb3d0e35e37f146f6c8616f55fea46a7345e3885
ozPop <ozziepopovas@gmail.com>
2016-10-04 17:15:47 -0400

    generate user controller, views, update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   root 'welcome#home'
 
-  get '/signin' => 'sessions#new'
-  post '/signin' => 'sessions#create'
-  post '/signout' => 'sessions#destroy'
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  post '/signout', to: 'sessions#destroy'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: :user
+  get '/users/:id/edit', to: 'users#edit', as: :user_edit
+  post '/users/:id/edit', to: 'users#update'
+  post '/users/:id/destroy', to: 'users#destroy'
 end
