2818b7778b1c049b5e7ef4dbf1bc7c1684129912
fidelscodes <fidelscodes@gmail.com>
2016-10-04 14:30:52 -0400

    All tests pass

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -3,7 +3,39 @@ class AttractionsController < ApplicationController
     @attractions = Attraction.all
   end
 
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.create(attraction_params)
+    if @attraction.save
+      redirect_to attraction_path(@attraction)
+    else
+      render 'new'
+    end
+  end
+
   def show
     @attraction = Attraction.find_by(id: params[:id])
+  end
+
+  def edit
+    @attraction = Attraction.find_by(id: params[:id])
+  end
+
+  def update
+    @attraction = Attraction.find_by(id: params[:id])
+    if @attraction.update(attraction_params)
+      redirect_to attraction_path(@attraction)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
   end
 end
