e219dba767b7c81a626c29f0e1256a634cf68dd4
ozPop <ozziepopovas@gmail.com>
2016-10-05 12:46:00 -0400

    update attractions controller actions, views

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -6,9 +6,14 @@ class AttractionsController < ApplicationController
   end
 
   def new
+    redirect_to attractions_path unless admin?
+    @attraction = Attraction.new
   end
 
   def create
+    @attraction = Attraction.new(attraction_params)
+    redirect_to :back unless @attraction.save
+    redirect_to attraction_path(@attraction)
   end
 
   def show
@@ -27,5 +32,9 @@ class AttractionsController < ApplicationController
 
   def set_attraction
     @attraction = Attraction.find(params[:id])
+  end
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
   end
 end
