85dfdd47daff0324625abb5cb1468c396675c5cb
parm530 <parmsahadeo530@gmail.com>
2016-10-24 16:58:19 -0400

    Done

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 # ruby '2.2.0'
 
+gem 'devise'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2'
 # Use SCSS for stylesheets
--- spec/models/attraction_spec.rb
+++ spec/models/attraction_spec.rb
@@ -22,8 +22,9 @@ RSpec.describe Attraction, :type => :model do
   end
 
   it "has many users through rides" do
-    max = User.create(name: "Max Charles")
-    skai = User.create(name: "Skai Jackson")
+    #added passwords to max and skai
+    max = User.create(name: "Max Charles", password: "password1")
+    skai = User.create(name: "Skai Jackson", password: "password2")
     @attraction.users << [max, skai]
     expect(@attraction.users.first).to eq(max)
     expect(@attraction.users.last).to eq(skai)
